f86ebc02f4cdde0b7fe97e491487ebc25d7c8945
SirJimKe <jimmymutuku1@gmail.com>
2022-09-20 16:18:33 +0300

    Completed Assignment

---
+++ src/Hogwarts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import whoseHouse from "./houses/whoseHouse";
+
+function Hogwarts() {
+    whoseHouse();
+
+    return <h1>Welcome to Hogwarts!</h1>
+}
+
+export default Hogwarts;
